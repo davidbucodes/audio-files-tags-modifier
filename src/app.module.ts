ceccbaf5af4d8fbe400b2e2848d58a678e49b88a
david <125945778+davidbucodes@users.noreply.github.com>
2023-05-07 11:01:13 +0300

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExplorerModule } from './explorer/explorer.module';
+import { FileModule } from './file/file.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [ExplorerModule, FileModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
